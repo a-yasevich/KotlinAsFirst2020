77a353b57a966108099e3d098b6ee5e17a10cb03
Artem <artem.yasevich@mail.ru>
2020-10-03 22:47:56 +0200

    lesson 6

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -316,6 +316,11 @@ class Tests {
             Pair(-1, -1),
             findSumOfTwo(listOf(1, 2, 3), 6)
         )
+        //added
+        assertEquals(
+            Pair(1, 2),
+            findSumOfTwo(listOf(1, 0, 0, 0), 0)
+        )
     }
 
     @Test
